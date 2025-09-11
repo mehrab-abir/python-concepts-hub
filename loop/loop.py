c092d4acefa7d3b3e65d602406c0bf2026976595
Mehrab-Abir <mjabir@myseneca.ca>
2025-09-11 19:13:03 -0400

    update

--- loop/loop.py
+++ loop/loop.py
@@ -1,18 +1,24 @@
 nums = [2,6,3,4,8,1]
 
-for num in nums:
-    print(num)
+""" for num in nums:
+    print(num) """
 
 print(f"Length of the list 'nums': {len(nums)}")
 
 print("************")
 #print in one line, seperated by comma
-for num in range(0,10,1):
-    print(num, end=", ")
+""" for num in range(0,10,1):
+    print(num, end=", ") """
 
 print("**********************")
 
-nums = [str(i) for i in range(1,15,1)]
-print(", ".join(nums))
+""" nums = [str(i) for i in range(1,15,1)]
+print(", ".join(nums)) """
+
+print("**********************")
+
+#to access the index, in this case, access values using index as well
+for i in range(len(nums)-1):
+    print(f"{i} -> {nums[i]}")
 
 
