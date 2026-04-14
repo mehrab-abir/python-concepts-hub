96f8a8cfc1974a1f429f8cc6f516db7c92706260
mehrab-abir <mehrababir100@gmail.com>
2026-04-14 14:54:08 -0400

    ...

--- Phitron/M2/max_min.py
+++ Phitron/M2/max_min.py
@@ -1,6 +1,6 @@
 ## maximum number among 3 numbers
 
-nums = list(map(int,input("Enter 3 numbers: ").split()))
+""" nums = list(map(int,input("Enter 3 numbers: ").split()))
 
 num_1 = nums[0]
 num_2 = nums[1]
@@ -12,7 +12,22 @@ elif((num_2 >= num_1) and (num_2 >= num_3)):
     print(f"Maximun is {num_2}")
 else:
     print(f"Maximum is {num_3}")
+     """
     
+# print digits from right to left
+
+t = int(input("Number of test cases: "))
+
+for i in range(t):
+    num = int(input("Enter a number: "))
     
-    
-## min is same
+    if(num == 0):
+        print(num)
+    else:
+        while num != 0:
+                digit = num % 10
+                print(digit,end=" ")
+                num = int(num / 10)
+                # num //=10 # same as the line above
+        print()
+                
